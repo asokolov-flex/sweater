fa943e04449e66043d5a43b156bf5a2fbfef5e35
asokolov <a.sokolov@flex-solution.com>
2021-12-28 20:47:22 +0200

    Init project with mustache

---
+++ src/main/java/com/firesolution/sweater/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.firesolution.sweater.repo;
+
+import com.firesolution.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
